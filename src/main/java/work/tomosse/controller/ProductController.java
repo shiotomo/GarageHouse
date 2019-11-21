bc6bf331d7e1681a979b6f98063464c96e546535
shiotomo <t.shiota16@gmail.com>
2019-11-21 15:19:52 +0900

    詳細用のコントローラを作成する

--- src/main/java/work/tomosse/controller/ProductController.java
+++ src/main/java/work/tomosse/controller/ProductController.java
@@ -9,10 +9,27 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ProductController {
 
+    /**
+     * Productの一覧を表示する
+     *
+     * @param mav
+     * @return
+     */
     @GetMapping
     public ModelAndView index(final ModelAndView mav) {
         mav.setViewName("product/index");
         return mav;
     }
 
+    /**
+     * Productの詳細を表示する
+     *
+     * @param mav
+     * @return
+     */
+    @GetMapping("/:product_id")
+    public ModelAndView show(final ModelAndView mav) {
+        mav.setViewName("product/show");
+        return mav;
+    }
 }
