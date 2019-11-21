aae2a95694371ccc03af5e2e367f14c15ded4391
shiotomo <t.shiota16@gmail.com>
2019-11-21 15:45:31 +0900

    Account一覧ページを作成した

---
+++ src/main/java/work/tomosse/controller/AccountController.java
@@ -0,0 +1,31 @@
+package work.tomosse.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import work.tomosse.service.AccountService;
+
+@RequestMapping("/account")
+@Controller
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    /**
+     * Account一覧を表示する
+     *
+     * @param mav
+     * @return
+     */
+    @GetMapping
+    public ModelAndView index(final ModelAndView mav) {
+        final var accountList = accountService.selectAll();
+        mav.addObject("accountList", accountList);
+        mav.setViewName("account/index");
+        return mav;
+    }
+}
