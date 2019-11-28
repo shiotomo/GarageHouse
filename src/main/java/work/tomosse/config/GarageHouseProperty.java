db2c164796aca82a9bf2bb747b8533ba80f81a68
shiotomo <t.shiota16@gmail.com>
2019-11-29 01:29:28 +0900

    propertyファイルの設定を行った

--- src/main/java/work/tomosse/config/GarageHouseProperty.java
+++ src/main/java/work/tomosse/config/GarageHouseProperty.java
@@ -1,5 +1,27 @@
 package work.tomosse.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.Getter;
+
+@Getter
 public class GarageHouseProperty {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.messages.basename}")
+    private String basename;
+
+    @Value("${spring.security.secret-key}")
+    private String secretKey;
 }
