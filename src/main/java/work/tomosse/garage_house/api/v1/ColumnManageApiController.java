88ba0fc142a5c9b4438752dba1f12f234fec77c1
shiotomo <t.shiota16@gmail.com>
2019-12-24 21:16:20 +0900

    リファクタリングを行った

--- src/main/java/work/tomosse/garage_house/api/v1/ColumnManageApiController.java
+++ src/main/java/work/tomosse/garage_house/api/v1/ColumnManageApiController.java
@@ -3,6 +3,7 @@ package work.tomosse.garage_house.api.v1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import work.tomosse.garage_house.service.ColumnManageService;
 @Api(tags = "ColumnManage")
 @ApiModel(value = "ColumnManage", description = "ColumnManage REST API")
 @RestController
-@RequestMapping("/api/v1/product/{prodcutId}/column_manages")
+@RequestMapping("/api/v1/products/{productId}/column_manages")
 public class ColumnManageApiController {
 
     @Autowired
@@ -27,6 +28,7 @@ public class ColumnManageApiController {
      * @param productId
      * @return
      */
+    @GetMapping
     public List<ColumnManage> getColumnManage(@PathVariable final Long productId) {
         return columnManageService.selectByProductId(productId);
     }
